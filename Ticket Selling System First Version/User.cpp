06ee9f76a5a00e9e2d316b86f08e048622d22cdf
root <abhiram.sinnarajah@uoit.net>
2019-02-06 16:24:08 -0500

    implemented multiple methods

--- Ticket Selling System First Version/User.cpp
+++ Ticket Selling System First Version/User.cpp
@@ -18,8 +18,8 @@ User::User(string username, string userAccountType, float availableCredit) {
 }
 
 string User::stringRepresentation(string username, string userType, float userCredit) {
-    string code;
-    string credit = (float) userCredit;
+    string transactionCode;
+    string credit = to_string(userCredit);
     code = username + " " + userType + " " + credit;
     return code;
 }
@@ -51,20 +51,22 @@ void User::createUser() {
 
 void User::deleteUser() {
  if (this.userAccountType == "AA") {
-   string name;
+   string name, transactionCode;
    cout << "Enter the username you wish to remove: " << endl;
    cin >>  name;
 
    // how to check where the outstanding ticket sales and purchase are for this user
-   if (checkUserExists == true)      // a check to see if this user has zero outsanding tickets
+   if (checkUserExists(name) == true)      // a check to see if this user has zero outsanding tickets
    {
      // invoke method that removes specified user's information
      // for a specified file, in this case the available tickets file
 
+     transactionCode = " ";
      // call method to pass information into the dtf
    }
    // if user does have outstanding tickets
    else {
+     transactionCode = " ";
      // then apply lines of code to make change to the dtf
    }
  }
@@ -77,22 +79,70 @@ void User::deleteUser() {
 void User::sellTickets() {
   //ignore this, this is just a side note for Jude for later:
   //dt.transactioncode = 02; dt.username = username...etc;
-
-
   if (this.userAccountType != "BS") {
+    string eventName, transactionCode;
+    uint numTickets;
+    float ticketPrice;
 
+    cout << "What is the name of the Event: " << endl;
+    cin >> eventName;
 
+    cout << "Number of tickets you wish to purchase: " << endl;
+    cin >> numTickets;
 
+    cout << "What is your preferred price for these tickets: " << endl;
+    cin >> ticketPrice;
+
+    // applied changes in credit for the seller should increase, question is
+    // where does this increase go to, user accounts file? need to check **
+
+    transactionCode = " ";
+    // then apply lines of code to make change to the dtf
   }
   else {
-    cout << "Sorry not a semi-privileged user, you don't have the ""
-    << "privilege to delete a current user" << endl;
+    cerr << "Sorry Buy standard (BS) users don't have the "
+    << "privilege to sell tikets to users." << endl;
   }
   //return dt.stringRepresentation;
 }
 
+void User::buyTickets() {
+  if (this.userAccountType != "SS") {
+    string eventName, transactionCode, sellerName, strNumTickets, strTicketPrice;
+    uint numTickets;
+    // need to invode method to retreive price of seller's ticket
+    // defaulted to 5 dollars for simplicity
+    float ticketPrice = 5.00;
+
+    cout << "Welcome to the Buyer's Terminal!"
+    cout << "What is the name of the Event: " << endl;
+    cin >> eventName;
+
+    cout << "Number of tickets you wish to purchase: " << endl;
+    cin >> numTickets;
+
+    cout << "Seller's name: " << endl;
+    cin >> sellerName;
+
+    // need to display to terminal price per tickete, and total price of tickets
+
+    // invoke method here to apply changes to user inventory
+    strNumTickets = to_string(userCredit);
+    strTicketPrice = to_string(ticketPrice);
+    transactionCode = "04 " + eventName + " " + sellerName + " " + strNumTickets
+    + " " + strTicketPrice;
+    // then apply lines of code to make change to the dtf
+  }
+  else {
+    cerr << "Sorry Sell standard (SS) users don't have the "
+    << "privilege to buy tikets from users." << endl;
+  }
+}
+
+
+
 // a function that checks in whether a user exists within a certain file (A.T or A.U)
-bool checkUserExists(string uname, File file) {
+bool checkUserExists(string uname) {
     // need to implement this function
     return false;
 }
