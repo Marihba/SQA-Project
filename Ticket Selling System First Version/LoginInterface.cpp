9b3a916c82adec2fc8cef9e98119844606669f03
Abhiram Sinnarajah <33011138+Marihba@users.noreply.github.com>
2019-02-06 08:44:38 -0500

    updating login interface with base methods

--- Ticket Selling System First Version/LoginInterface.cpp
+++ Ticket Selling System First Version/LoginInterface.cpp
@@ -1,1 +1,30 @@
 // this file contains information regarding user login
+#include <iostream>
+#include "LoginInterface.h"
+
+using namespace std;
+
+
+void LoginInterface::accessUserFile() {
+
+}
+
+void LoginInterface::scanForExistingUser() {
+
+}
+
+void LoginInterface::displayLogin() {
+
+}
+
+bool LoginInterface::isExistingUser() {
+
+}
+
+void LoginInterface::retrieveUserData() {
+
+}
+
+void LoginInterface::exitProgram() {
+
+}
