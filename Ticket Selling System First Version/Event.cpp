96b31b4bec934d6054c9d96caed070915a22403b
root <abhiram.sinnarajah@uoit.net>
2019-02-07 10:44:29 -0500

    added methods and defined Event class

--- Ticket Selling System First Version/Event.cpp
+++ Ticket Selling System First Version/Event.cpp
@@ -1,6 +1,13 @@
 // contains information for an Event
-
-
 #include <iostream>
 
 using namespace std;
+
+// constructor methods
+Event::Event(string name, string host, unsigned int numTickets {
+  this->eventName = name;
+  this->hostName = host;
+  this->ticketsAvailable = numTicket;
+}
+
+// Event class methods
---
+++ Ticket Selling System First Version/Event.h
@@ -0,0 +1,20 @@
+// header file for the event class
+using namespace std;
+
+class Event {
+  // member variables
+  private:
+    string eventName;
+    string hostName;
+    unsigned int ticketsAvailable;
+  public:
+    // constructor
+    Event(string name, string host, unsigned int numTicket);
+
+    // Event class methods
+
+}
+
+// REMINIDER** JUDE look at this after your done your function
+// what is the purpose of the event class? if we are to be simply checking
+// and updating information on files like available tickets
